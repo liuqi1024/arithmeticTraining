9c5fd29ca7bde028c682027c725ed6a2baa06006
liuqi1024 <keyboarder5211@gmail.com>
2019-04-01 11:29:21 +0800

    init project

---
+++ src/com/cslp/ken/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.cslp.ken;
+
+/**
+ * @author liuqi
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
